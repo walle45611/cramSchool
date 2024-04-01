d53df6566e286fab8abe49dc6bbd10a87d580ef6
walle45611 <11146001@ntub.edu.tw>
2024-04-01 17:43:53 +0800

    (refactor): 重新定義專案名稱

--- cramSchool/wsgi.py
+++ cramSchool/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cramSchool project.
+WSGI config for cramschool project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cramSchool.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cramschool.settings')
 
 application = get_wsgi_application()
